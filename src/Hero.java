9fcd0a7d17d54646bec9e56ab106f0fc4a1a7836
Alan Tse <starttodevelop@gmail.com>
2017-05-04 15:44:09 +1000

    Updated the Hero class

--- src/Hero.java
+++ src/Hero.java
@@ -23,15 +23,25 @@ public class Hero {
 		generateAbilities();
 	}
 	
+	public void showAbilities() {
+		System.out.print("+++++++++++++++++++++++++++++++++");
+		System.out.print("Strength: " + this._strength);
+		System.out.print("Speed: " + this._speed);
+		System.out.print("Health: " + this._health);
+		System.out.print("+++++++++++++++++++++++++++++++++");
+	}
+	
 	private void generateAbilities() {
-		
+		this._strength = (int)(Math.random()*100 + 1);
+		this._speed = (int)(Math.random()*100 + 1);
+		this._health = (int)(Math.random()*100 + 1);
 	}
 	
 	public void fight() {
-
+		System.out.print(this._name + " is Fighting!");
 	}
 
 	public void run() {
-
+		System.out.print(this._name + " is Running!");
 	}
 }
